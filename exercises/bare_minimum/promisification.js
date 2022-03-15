85acc1e69561d4b2a8f86df88988e42df6036323
Andy Chan <Chanandy727@gmail.com>
2022-03-15 16:25:57 -0700

    passed promisification

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -29,27 +29,29 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync = Promise.Promisify(this.getGitHubProfileAsync);
+
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
-var generateRandomToken = function(callback) {
-  crypto.randomBytes(20, function(err, buffer) {
+var generateRandomToken = function (callback) {
+  crypto.randomBytes(20, function (err, buffer) {
     if (err) { return callback(err, null); }
     callback(null, buffer.toString('hex'));
   });
 };
 
-var generateRandomTokenAsync; // TODO
+
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
-var readFileAndMakeItFunny = function(filePath, callback) {
-  fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
+var readFileAndMakeItFunny = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, file) {
+    if (err) { return (err); }
 
     var funnyFile = file.split('\n')
-      .map(function(line) {
+      .map(function (line) {
         return line + ' lol';
       })
       .join('\n');
@@ -58,7 +60,38 @@ var readFileAndMakeItFunny = function(filePath, callback) {
   });
 };
 
-var readFileAndMakeItFunnyAsync; // TODO
+
+// This function violates rule (2) of the node style callback pattern,
+// therefore we have to reimplement it using the `new Promise` constructor
+var readFileAndMakeItFunnyAsync = function (filePath) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, 'utf8', function (err, file) {
+      if (err) { return reject(err); }
+
+      var funnyFile = file.split('\n')
+        .map(function (line) {
+          return line + ' lol';
+        })
+        .join('\n');
+
+      resolve(funnyFile);
+    });
+  });
+};
+
+// Alternatively, we could use our previously written function
+// and do some error checking on the only argument passed to the callback
+// readFileAndMakeItFunnyAsync = function (filePath) {
+//   return new Promise(function (resolve, reject) {
+//     readFileAndMakeItFunny(filePath, function (errorOrFile) {
+//       if (errorOrFile instanceof Error) {
+//         reject(errorOrFile);
+//       } else {
+//         resolve(errorOrFile);
+//       }
+//     });
+//   });
+// };
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
